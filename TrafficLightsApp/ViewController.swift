47a09051451c175fa1a5edcc583ebce05d599ec4
Ivan <ivnknv@zohomail.com>
2021-07-20 18:38:11 +0300

    Made app interface and add outlets and action on ViewController

--- TrafficLightsApp/ViewController.swift
+++ TrafficLightsApp/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redViewLight: UIImageView!
+    @IBOutlet weak var yellowViewLight: UIImageView!
+    @IBOutlet weak var greenViewLight: UIImageView!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func startButtonTapped() {
+    }
+    
 }
 
